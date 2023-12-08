23b1e753a737a96769c32ce3ab0415fa206b2ec9
Pierre Chojka <pchojka.ext@mediarithmics.com>
2023-12-08 14:25:50 +0100

    D5-2 (po fini wlh)

--- d5/maps.py
+++ d5/maps.py
@@ -8,13 +8,19 @@ class RangeMap:
         self.src_start_range = src_range_start
         self.range_len = range_len
         self.result = []
+        self.min_op_range = {}
+        self.is_complete = True
 
-    # def process(self):
-    #     for i in range(0,self.range_len):
-    #         self.result.append({
-    #             'src': self.src_start_range+i,
-    #             'dst': self.dst_start_range+i
-    #         })
+    def process(self):
+        self.min_op_range = min(self.result, key=operator.itemgetter('op'))
+        if self.result[0]['src_min'] != 0:
+            self.is_complete = False
+            return
+        for i in range(0, len(self.result) - 1):
+            if self.result[i]['src_max'] != self.result[i+1]['src_min'] -1:
+                self.is_complete = False
+                return
+        
             
     def add_sub_range(self, range):
         self.result.append({
@@ -37,13 +43,51 @@ class RangeMap:
             'type': self.dst_type,
             'value': input_value
         }
+    def process_app_ranges(self, input_min, input_max):
+        ranges = []
+        for range in self.result:
+            if input_min >= range['src_min'] and input_max <= range['src_max']:
+                return [range]
+            if input_max < range['min_src'] or input_min > range['max_src']:
+                continue
+            if input_min >= range['src_min'] and input_max >= range['src_max']:
+                ranges.append(range)
+            if input_min < range['src_min'] and input_max < range['src_max']:
+                ranges.append({
+                    'src_min': range['src_min'],
+                    'src_max': input_max,
+                    'op': range['op']
+                })
+        return ranges
     
+
+    def process_input_range(self, input_type:str, input_min_value:int, input_max_value:int):
+            result = []  
+            if input_type != self.source_type:
+                raise TypeError('Input type is not expected')
+            applicable_ranges = self.process_app_ranges(input_min_value, input_max_value)
+            match (len(applicable_ranges)):
+                case 0:
+                    result.append({
+                        'type': self.dst_type,
+                        'min_value': input_min_value,
+                        'max_value': input_max_value
+                    })
+                case 1:
+                    result.append({
+                        'type': self.dst_type,
+                        'min_value': input_min_value+ applicable_ranges[0]['op'],
+                        'max_value': input_max_value + applicable_ranges[0]['op']
+                    })
+            return result
+            
     def __repr__(self) -> str:
         res = ""
         res += f"{self.source_type} => {self.dst_type}\n"
         for range in self.result:
-             res+= f"SRC : {range['src_min']} => {range['src_max']}\n"
-             res+= f"DST : {range['src_min'] + range['op']} => {range['src_max'] + range['op']}\n\n"
+             res+= f"SRC : {range['src_min']} => {range['src_max']} ; Op : {range['op']}\n"
+        res+= f"Min_ops_range: {self.min_op_range['src_min']} => {self.min_op_range['src_max']} ; Op : {self.min_op_range['op']}\n"
+
         return res
 
 
--- d5/script.py
+++ d5/script.py
@@ -1,19 +1,43 @@
 from maps import RangeMap
 import sys
 
+def process_seeds(seeds):
+    arr = []
+    for i in range(0, len(seeds)-1, 2):
+        arr.append({
+            'type': 'seed',
+            'min_value': int(seeds[i]),
+            'max_value': int(seeds[i]) + int(seeds[i+1]) - 1
+        })
+    return arr
+
+def process_pivot_value(seed_value, maps):
+    payload = {
+        'type': 'seed',
+        'value': int(seed_value)
+    }
+    map: RangeMap
+    for map in maps:
+        payload = map.process_input(payload['type'], payload['value'])
+        if payload['type'] == 'location':
+            return payload['value']
+
+
 
 def script():
     seeds = []
     maps : [RangeMap] = []
     current_map: RangeMap = None
     locations : [int] = []
-    with open('input.txt', 'r') as readfile:
+    with open('test.txt', 'r') as readfile:
         lines = readfile.readlines()
         count = 1
         for line in lines:
             try:
                 if line.count('seeds') != 0:
                     seeds = [d for d in str(line.replace('\n', '').split(': ')[1]).split(' ')]
+                    seeds = process_seeds(seeds)
+                    print(seeds)
                 elif line == '\n':
                     if current_map != None:
                         maps.append(current_map)
@@ -29,19 +53,27 @@ def script():
                     current_map.add_sub_range(range)
             except TypeError as err:
                 print(f"ERROR on line {count} : {err}")
-        for seed in seeds:
-            payload = {
-                'type': 'seed',
-                'value': int(seed)
-            }
-            print(payload)
-            for map in maps:
-                payload = map.process_input(payload['type'], payload['value'])
-                print(payload)
-                if payload['type'] == 'location':
-                    locations.append(payload['value'])
+            
+            map: RangeMap
+        for map in maps:
+            map.process()
+        print(maps)
 
-    print(min(locations))
+        pivot = process_pivot_value(seeds[0]['min_value'], maps)
+        print(f"PIVOT VALUE : {pivot}")
+
+        for seed in seeds:
+            for map in maps:
+                payload = map.process_input_range(seed['type'], seed['min_value'], seed['max_value']) 
+        # for seed in seeds:
+        #     for map in maps:
+                
+        #         payload = map.process_input(payload['type'], payload['value'])
+        #         print(payload)
+        #         if payload['type'] == 'location':
+        #             locations.append(payload['value'])
+
+    #print(min(locations))
 
 
 script()
